6fe2f9260cf6a0d044dbbdcb979a00314fb94cf2
Vadim Tkachenko <vadim@percona.com>
2009-04-07 14:42:16 -0700

    fix for build script, patch to patches

--- utils/buildtree.sh
+++ utils/buildtree.sh
@@ -9,13 +9,13 @@ DRNAME=`basename $PATHS .tar.gz`
 mkdir -p $DIRDST/xtrabackup-build
 
 tar xfz $PATHS -C $DIRDST/xtrabackup-build
-cp fix_innodb_for_backup.patch $DIRDST/xtrabackup-build/$DRNAME
+cp ../fix_innodb_for_backup.patch $DIRDST/xtrabackup-build/$DRNAME
 mkdir -p  $DIRDST/xtrabackup-build/$DRNAME/innobase/xtrabackup
-cp * $DIRDST/xtrabackup-build/$DRNAME/innobase/xtrabackup 
+cp ../* $DIRDST/xtrabackup-build/$DRNAME/innobase/xtrabackup 
 pushd $DIRDST/xtrabackup-build/$DRNAME
 patch -p1 < fix_innodb_for_backup.patch
 
-#./configure
-#make -j8
-#cd innobase/xtrabackup
-#make
+./configure
+make -j8
+cd innobase/xtrabackup
+make
