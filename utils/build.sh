b9f8c6e0c616f9903c33cb6333b7e61da63c4ff6
Ignacio Nin <ignacio.nin@percona.com>
2011-04-09 21:44:02 -0300

    Update packaging to release 1.6
    Added scripts for compiling RPMs and debs.
    Fixed versions in building files (spec, debian/rules, build.sh).
    Added execution flag to rules.

--- utils/build.sh
+++ utils/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-MYSQL_51_VERSION=5.1.55
-MYSQL_55_VERSION=5.5.9
+MYSQL_51_VERSION=5.1.56
+MYSQL_55_VERSION=5.5.10
 
 AUTO_DOWNLOAD=${AUTO_DOWNLOAD:-no}
 MASTER_SITE="http://www.percona.com/downloads/community/"
