e0a45fba6637a336e7b8204ea0056f3efb1efda1
Valentine Gostev <core.longbow@gmail.com>
2011-04-18 06:40:32 +0700

    Added test for bug 723318

---
+++ test/t/bug723318.sh
@@ -0,0 +1,26 @@
+. inc/common.sh
+
+init
+run_mysqld
+load_dbase_schema sakila
+load_dbase_data sakila
+
+# Take backup
+mkdir -p $topdir/backup
+mkdir -p $topdir/backup/stream
+run_cmd ${IB_BIN} --socket=$mysql_socket --stream=tar $topdir/backup > $topdir/backup/stream/out.tar 2> $OUTFILE 
+
+stop_mysqld
+cd $topdir/backup/stream/
+tar -ixvf out.tar
+
+if [ -f $topdir/backup/stream/xtrabackup_binary ]
+then
+	vlog "File xtrabackup_binary was copied"
+else
+	vlog "File xtrabackup_binary was not copied"
+	exit -1
+fi
+
+cd - >/dev/null 2>&1 
+clean
