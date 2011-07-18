921aacc9945e1fee1f311778ae68a6b114c2068a
Alexey Kopytov <akopytov@gmail.com>
2011-07-18 13:30:58 +0400

    Bug #665210: tar4ibd does not support innodb row_format=compressed Bug #810269: tar4ibd does not check for doublewrite buffer pages
    1. Merged the fix proposed by Andrew Garner in bug #665210 which adds
    support for compressed tables to tar4ibd.
    
    2. Added checks for doublewrite buffer pages to tar4ibd to fix bug
    #810269.

---
+++ test/t/bug810269.sh
@@ -0,0 +1,82 @@
+########################################################################
+# Bug #665210: tar4ibd does not support innodb row_format=compressed
+# Bug #810269: tar4ibd does not check for doublewrite buffer pages
+########################################################################
+
+. inc/common.sh
+
+init
+
+if [ -z "$INNODB_VERSION" ]; then
+    echo "Requires InnoDB plugin or XtraDB" >$SKIPPED_REASON
+    exit $SKIPPED_EXIT_CODE
+fi
+
+run_mysqld "--innodb_strict_mode --innodb_file_per_table \
+--innodb_file_format=Barracuda"
+
+load_dbase_schema incremental_sample
+
+vlog "Compressing the table"
+
+run_cmd $MYSQL $MYSQL_ARGS -e \
+    "ALTER TABLE test ENGINE=InnoDB ROW_FORMAT=compressed \
+KEY_BLOCK_SIZE=4" incremental_sample
+
+vlog "Adding initial rows to table"
+
+numrow=10000
+count=0
+while [ "$numrow" -gt "$count" ]; do
+    sql="INSERT INTO test VALUES ($count, $numrow)"
+    let "count=count+1"
+    for ((i=0; $i<99; i++)); do
+	sql="$sql,($count, $numrow)"
+	let "count=count+1"
+    done
+    ${MYSQL} ${MYSQL_ARGS} -e "$sql" incremental_sample
+done
+
+rows=`${MYSQL} ${MYSQL_ARGS} -Ns -e "SELECT COUNT(*) FROM test" \
+    incremental_sample`
+if [ "$rows" != "10000" ]; then
+    vlog "Failed to add initial rows"
+    exit -1
+fi
+
+vlog "Initial rows added"
+
+checksum_a=`${MYSQL} ${MYSQL_ARGS} -Ns -e "checksum table test;" \
+incremental_sample | awk '{print $2}'`
+
+vlog "Starting streaming backup"
+
+mkdir -p $topdir/backup
+
+innobackupex --stream=tar $topdir/backup > $topdir/backup/out.tar
+
+stop_mysqld
+rm -rf $mysql_datadir
+
+vlog "Applying log"
+
+cd $topdir/backup
+$TAR -ixvf out.tar
+cd -
+innobackupex --apply-log $topdir/backup
+
+vlog "Restoring MySQL datadir"
+mkdir -p $mysql_datadir
+innobackupex --copy-back $topdir/backup
+
+run_mysqld
+
+checksum_b=`${MYSQL} ${MYSQL_ARGS} -Ns -e "checksum table test;" \
+incremental_sample | awk '{print $2}'`
+
+if [ "$checksum_a" != "$checksum_b" ]; then
+    vlog "Checksums do not match"
+    exit -1
+fi
+
+vlog "Checksums are OK"
