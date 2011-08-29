37868f5683153a0a723a91c78e05b957847171bb
Alexey Kopytov <akopytov@gmail.com>
2011-08-30 01:44:10 +0600

    Bug #834657: --slave-info does not work with --no-lock
    Moved the call to write_slave_info() out of mysql_lockall() to make
    --slave-info work with --no-lock or --incremental when
    --safe-slave-backup is also specified.
    
    In addition, an attempt to use --slave-info with --no-lock or
    --incremental but without --safe-slave-backup will result in an error.

---
+++ test/t/ib_slave_info.sh
@@ -0,0 +1,28 @@
+. inc/common.sh
+
+init
+run_mysqld --innodb_file_per_table
+load_dbase_schema incremental_sample
+
+# Adding initial rows
+vlog "Adding initial rows to database..."
+numrow=100
+count=0
+while [ "$numrow" -gt "$count" ]
+do
+	${MYSQL} ${MYSQL_ARGS} -e "insert into test values ($count, $numrow);" incremental_sample
+	let "count=count+1"
+done
+vlog "Initial rows added"
+
+# Full backup
+# backup root directory
+mkdir -p $topdir/backup
+
+vlog "Check that --slave-info with --no-lock and no --safe-slave-backup fails"
+run_cmd_expect_failure $IB_BIN $IB_ARGS --slave-info --no-lock $topdir/backup
+
+vlog "Check that --slave-info with --incremental and no --safe-slave-backup fails"
+run_cmd_expect_failure $IB_BIN $IB_ARGS --slave-info --incremental --incremental-basedir=$topdir/backup $topdir/backup
+
+# TODO: add positive tests when the test suite is able to setup replication
