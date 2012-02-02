0ebf2678d57a0cc132b3b20fbf7d43e1fd19a87d
Alexey Kopytov <akopytov@gmail.com>
2012-02-03 00:15:20 +0400

    Cherrypick of (C) string update due to 2012.

--- xb_regex.h
+++ xb_regex.h
@@ -1,5 +1,5 @@
 /******************************************************
-Copyright (c) 2011 Percona Inc.
+Copyright (c) 2011-2012 Percona Inc.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
