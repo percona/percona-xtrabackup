0a1f7cfd78d962f0c3699dcd903ce72f6ef0b5fe
Hrvoje Matijakovic <hrvoje.matijakovic@percona.com>
2012-02-08 16:08:50 +0100

    added release notes for XB-1.6.5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Percona XtraBackup'
-copyright = u'2012, Percona Inc'
+copyright = u'2010-2012, Percona Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
