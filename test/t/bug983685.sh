3d0950c6f2521e98c8d57b72042e5f7f61292475
Sergei Glushchenko <sergei.glushchenko@percona.com>
2012-04-17 19:55:16 +0700

    Bug 983685: innodb_data_file_path is not written to backup-my.cnf Regression introduced by fix for bug #733651. Along with innodb_data_file_path, innodb_log_group_home_dir is also missed.

---
+++ test/t/bug983685.sh
@@ -0,0 +1,28 @@
+########################################################################
+# Bug #983685: innodb_data_file_path is not written to backup-my.cnf
+########################################################################
+
+. inc/common.sh
+
+init
+
+options="innodb_data_file_path innodb_log_group_home_dir"
+
+run_mysqld
+
+mkdir -p $topdir/backup
+innobackupex  $topdir/backup
+backup_dir=`grep "innobackupex: Backup created in directory" $OUTFILE | awk -F\' '{ print $2}'`
+vlog "Backup created in directory $backup_dir"
+
+# test presence of options
+for option in $options ; do
+
+	if ! grep $option $backup_dir/backup-my.cnf ; then
+		vlog "Option $option is absent"
+		exit -1
+	else
+		vlog "Option $option is present"
+	fi
+
+done
