a3b98a82f4d8609d4d85e6ad41c260220fe80df1
Sergei Glushchenko <sergei.glushchenko@percona.com>
2012-04-20 13:59:15 +0700

    Bug #983720: ib_lru_dump and --galera-info fail with --stream=xbstream innobackupex called the tar utility unconditionally when streaming ib_lru_dump and xtrabackup_galera_info. Which led to a broken stream when the xbstream format is used. Fix is to use $stream_cmd instead of calling the hard-coded "tar" utility.

---
+++ test/t/bug983720_galerainfo.sh
@@ -0,0 +1,32 @@
+############################################################################
+# Bug983720: ib_lru_dump and --galera-info fail with --stream=xbstream
+############################################################################
+
+. inc/common.sh
+
+set +e
+${MYSQLD} --basedir=$MYSQL_BASEDIR --user=$USER --help --verbose --wsrep-sst-method=rsync| grep -q wsrep
+probe_result=$?
+if [[ "$probe_result" == "0" ]]
+    then
+        vlog "Server supports wsrep"
+    else
+        echo "Requires WSREP enabled" > $SKIPPED_REASON
+        exit $SKIPPED_EXIT_CODE
+fi
+set -e
+
+init
+run_mysqld --log-bin=`hostname`-bin --binlog-format=ROW --wsrep-provider=${MYSQL_BASEDIR}/libgalera_smm.so --wsrep_cluster_address=gcomm://
+
+# take a backup with stream mode
+mkdir -p $topdir/backup
+innobackupex --galera-info --stream=xbstream $topdir/backup > $topdir/backup/stream.xbs
+
+xbstream -xv -C $topdir/backup < $topdir/backup/stream.xbs
+if [ -f $topdir/backup/xtrabackup_galera_info ] ; then
+    vlog "galera info has been backed up"
+else
+    vlog "galera info has not been backed up"
+    exit -1
+fi
---
+++ test/t/bug983720_lrudump.sh
@@ -0,0 +1,28 @@
+############################################################################
+# Bug983720: ib_lru_dump and --galera-info fail with --stream=xbstream
+############################################################################
+
+. inc/common.sh
+
+if [ -z "$XTRADB_VERSION" ]; then
+    echo "Requires XtraDB" > $SKIPPED_REASON
+    exit $SKIPPED_EXIT_CODE
+fi
+
+init
+run_mysqld
+
+# produce ib_lru_dump
+${MYSQL} ${MYSQL_ARGS} -e "select * from information_schema.XTRADB_ADMIN_COMMAND /*!XTRA_LRU_DUMP*/;"
+
+# take a backup with stream mode
+mkdir -p $topdir/backup
+innobackupex --stream=xbstream $topdir/backup > $topdir/backup/stream.xbs
+
+xbstream -xv -C $topdir/backup < $topdir/backup/stream.xbs
+if [ -f $topdir/backup/ib_lru_dump ] ; then
+    vlog "LRU dump has been backed up"
+else
+    vlog "LRU dump has not been backed up"
+    exit -1
+fi
