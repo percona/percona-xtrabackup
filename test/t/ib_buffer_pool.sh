0c04031b1f1b47618c7414d91b035674c26d4992
Sergei Glushchenko <sergei.glushchenko@percona.com>
2013-04-25 21:32:09 +0700

    Blueprint: Support InnoDB buffer pool dumps in MySQL 5.6 https://blueprints.launchpad.net/percona-xtrabackup/+spec/support-56-buffer-pool-dumps MySQL 5.6 adds an ability to dump the buffer pool into file for it to be possible to load it into memory at startup or on demand. The location of buffer pool file is set by global variable innodb_buffer_pool_filename and is relative to MySQL data directory. The patch adds an ability to take a backup of buffer pool dump. It has been made exactly the same way as XtraDB LRU dump is backed up. Three tests have been added for this feature. One takes regular backup and after takes streaming backup. Another one takes backup with --rsync option. Third test tests feature with incremental backups.

---
+++ test/t/ib_buffer_pool.sh
@@ -0,0 +1,57 @@
+########################################################################
+# Blueprint: Support InnoDB buffer pool dumps in MySQL 5.6
+########################################################################
+
+. inc/common.sh
+
+if [ "${MYSQL_VERSION:0:3}" != "5.6" ]; then
+    echo "Requires MySQL 5.6" > $SKIPPED_REASON
+    exit $SKIPPED_EXIT_CODE
+fi
+
+MYSQLD_EXTRA_MY_CNF_OPTS="
+innodb_buffer_pool_filename=pool/dump
+"
+
+start_server
+
+# test presence of innodb_buffer_pool_filename
+${XB_BIN} --defaults-file=$topdir/my.cnf --print-param | \
+                            grep -q "innodb_buffer_pool_filename"
+
+mkdir $mysql_datadir/pool
+
+# produce buffer pool dump
+${MYSQL} ${MYSQL_ARGS} -e "SET GLOBAL innodb_buffer_pool_dump_now=ON;"
+
+# take a backup
+innobackupex --no-timestamp $topdir/backup
+
+if [ -f $topdir/backup/pool/dump ] ; then
+    vlog "Buffer pool dump has been backed up"
+else
+    vlog "Buffer pool dump has not been backed up"
+    exit -1
+fi
+
+# take streaming backup
+mkdir -p $topdir/backup
+innobackupex --stream=tar $topdir/backup > $topdir/backup/stream.tar
+
+if $TAR itf $topdir/backup/stream.tar | grep 'pool/dump' ; then
+    vlog "Buffer pool dump has been restored"
+else
+    vlog "Buffer pool dump has not been restored"
+    exit -1
+fi
+
+# restore from backup
+rm -rf $mysql_datadir/*
+innobackupex --copy-back $topdir/backup
+
+if [ -f $mysql_datadir/pool/dump ] ; then
+    vlog "Buffer pool dump has been restored"
+else
+    vlog "Buffer pool dump has not been restored"
+    exit -1
+fi
