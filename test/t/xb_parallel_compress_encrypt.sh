2ea9a6652e343ddba0153d373fb64bcabfaa0bca
George Lorch <gl_az@yahoo.com>
2013-07-11 21:21:09 -0700

    Fix for lp1160778 : non-InnoDB files are not encrypted.
    Refactored some innobackupex to make all files not copied/streamed by xtrabackup binary go through one of two common functions. These new functions handle all of the stream/encryption formatting for non-InnoDB files. This also simplifies and normalizes some code in innobackupex that has just been added onto over time.
    
    Encryption test cases have also been fixed and improved. xb_encrypt.sh will now fail if there are any unencrypted files detected within the backup set.

--- test/t/xb_parallel_compress_encrypt.sh
+++ test/t/xb_parallel_compress_encrypt.sh
@@ -12,14 +12,10 @@ encrypt_key="percona_xtrabackup_is_awesome___"
 
 innobackupex_options="--parallel=8 --compress --compress-threads=4 --compress-chunk-size=8K --encrypt=$encrypt_algo --encrypt-key=$encrypt_key --encrypt-threads=4 --encrypt-chunk-size=8K"
 
-data_decrypt_cmd="for i in *.xbcrypt; do \
-xbcrypt -d -a $encrypt_algo -k $encrypt_key < \$i > \${i:0:\${#i}-8}; \
-rm -f \$i; done; \
-for i in ./sakila/*.xbcrypt; do \
-xbcrypt -d -a $encrypt_algo -k $encrypt_key < \$i > \${i:0:\${#i}-8}; \
+data_decrypt_cmd="for i in \`find \\\`pwd\\\` -name \*.xbcrypt\`; do \
+xbcrypt -d -a $encrypt_algo -k $encrypt_key -i \$i -o \${i:0:\${#i}-8}; \
 rm -f \$i; done;"
 
-data_decompress_cmd="for i in *.qp;  do qpress -d \$i ./; done; \
-for i in sakila/*.qp; do qpress -d \$i sakila/; done"
+data_decompress_cmd="for i in \`find \\\`pwd\\\` -name \*.qp\`; do qpress -d \$i \`dirname \$i\`; done;"
 
 . inc/xb_local.sh
--- test/t/xb_parallel_encrypt.sh
+++ test/t/xb_parallel_encrypt.sh
@@ -2,16 +2,12 @@
 # Test basic local parallel backup with encryption
 ############################################################################
 
-
 encrypt_algo="AES256"
 encrypt_key="percona_xtrabackup_is_awesome___"
 
 innobackupex_options="--parallel=4 --encrypt=$encrypt_algo --encrypt-key=$encrypt_key --encrypt-threads=4 --encrypt-chunk-size=8K"
-data_decrypt_cmd="for i in *.xbcrypt; do \
-xbcrypt -d -a $encrypt_algo -k $encrypt_key < \$i > \${i:0:\${#i}-8}; \
-rm -f \$i; done; \
-for i in ./sakila/*.xbcrypt; do \
-xbcrypt -d -a $encrypt_algo -k $encrypt_key < \$i > \${i:0:\${#i}-8}; \
+data_decrypt_cmd="for i in \`find \\\`pwd\\\` -name \*.xbcrypt\`; do \
+xbcrypt -d -a $encrypt_algo -k $encrypt_key -i \$i -o \${i:0:\${#i}-8}; \
 rm -f \$i; done;"
 
 . inc/xb_local.sh
