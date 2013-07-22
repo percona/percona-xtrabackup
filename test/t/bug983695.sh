1badd3f9175cad8764684631e19c7d4377b81eea
Alexey Kopytov <akopytov@gmail.com>
2013-07-22 16:38:55 +0400

    Bug #1042796: Test bug983695.sh failure
    Fixed the test to uncompress the backup before calling innobackupex
    --copy-back. Also added innodb_file_per_table for better coverage.

--- test/t/bug983695.sh
+++ test/t/bug983695.sh
@@ -9,13 +9,18 @@ if ! which qpress > /dev/null 2>&1 ; then
   exit $SKIPPED_EXIT_CODE
 fi
 
-start_server
+start_server --innodb_file_per_table
 load_sakila
 
 innobackupex --compress --no-timestamp $topdir/backup
 
 stop_server
 rm -rf ${MYSQLD_DATADIR}/*
+
+# Uncompress the backup
+cd $topdir/backup
+for i in *.qp sakila/*.qp;  do qpress -d $i ./; done;
+cd -
 
 innobackupex --copy-back $topdir/backup
 
