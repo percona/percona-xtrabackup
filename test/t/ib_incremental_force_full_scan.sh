20f7b2c07d4778668f471a0898cbc5ee4a080293
Alexey Kopytov <akopytov@gmail.com>
2013-08-20 21:51:19 +0400

    Bug #1204045: Enable xb_incremental_compressed_bitmap* tests for PXC
    Enabled back bitmap tests for PXC.

--- test/t/ib_incremental_force_full_scan.sh
+++ test/t/ib_incremental_force_full_scan.sh
@@ -2,19 +2,6 @@
 
 require_xtradb
 
-# The test is disabled for Percona XtraDB Cluster until a version
-# with bitmap user requests is released (containing PS 5.5.29-30.0 
-# or later)
-set +e
-${MYSQLD} --basedir=$MYSQL_BASEDIR --user=$USER --help --verbose --wsrep-sst-method=rsync| grep -q wsrep
-probe_result=$?
-if [[ "$probe_result" == "0" ]]
-    then
-        skip_test "Incompatible test"
-fi
-set -e
-
-
 MYSQLD_EXTRA_MY_CNF_OPTS="
 innodb-track-changed-pages=TRUE
 "
