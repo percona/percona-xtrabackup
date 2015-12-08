b831f9cc58bdae6b7fdc6f9e7e1b95ebd7db2b4d
Sergei Glushchenko <gl.sergei@gmail.com>
2015-12-08 07:44:36 +0700

    Bug 1523687: xtrabackup_binlog_info is not being updated correctly when applying incrementals
    Copy xtrabackup_binlog_info, xtrabackup_galera_info,
    xtrabackup_slave_info, xtrabackup_info from incremental directory
    over the corresponding files in full directory.

---
+++ storage/innobase/xtrabackup/test/t/bug1523687.sh
@@ -0,0 +1,32 @@
+#
+# Bug 1523687: xtrabackup_binlog_info is not being
+#              updated correctly when applying incrementals
+#
+
+start_server
+
+$MYSQL $MYSQL_ARGS -e "CREATE TABLE t (a INT) ENGINE=InnoDB" test
+$MYSQL $MYSQL_ARGS -e "INSERT INTO t VALUES (1), (2), (3)" test
+
+xtrabackup --backup --target-dir=$topdir/full
+
+$MYSQL $MYSQL_ARGS -e "INSERT INTO t VALUES (4), (5), (6)" test
+
+xtrabackup --backup --incremental-basedir=$topdir/full --target-dir=$topdir/inc
+
+xtrabackup --prepare --apply-log-only --target-dir=$topdir/full
+
+xtrabackup --prepare --apply-log-only --target-dir=$topdir/full \
+				      --incremental-dir=$topdir/inc
+
+xtrabackup --prepare --target-dir=$topdir/full
+
+# verify that following files overwritten in full backup directory with the
+# corresponding files from incremental directory
+for i in xtrabackup_binlog_info xtrabackup_galera_info \
+	 xtrabackup_slave_info xtrabackup_info ;
+do
+	if [ -f $topdir/inc/$i ] ; then
+		diff -u $topdir/full/$i $topdir/inc/$i
+	fi
+done
