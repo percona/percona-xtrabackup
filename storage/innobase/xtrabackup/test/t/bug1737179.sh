7c7f4cd22ac00f6262e177e5c0a9dc3b19210289
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-12-11 18:24:03 +0400

    Again trying to fix test N2

--- storage/innobase/xtrabackup/test/t/bug1737179.sh
+++ storage/innobase/xtrabackup/test/t/bug1737179.sh
@@ -15,11 +15,11 @@ vlog "Prepare full"
 
 xtrabackup --prepare --apply-log-only \
     --throttle=40 \
-    --target-dir=$topdir/data/full | tee $topdir/pxb.log
+    --target-dir=$topdir/data/full > tee $topdir/pxb.log 2>&1
 
 vlog "Prepare incremental"
 xtrabackup --prepare --apply-log-only \
     --throttle=40 \
-    --target-dir=$topdir/data/full --incremental-dir=$topdir/data/delta | tee $topdir/pxb.log
+    --target-dir=$topdir/data/full --incremental-dir=$topdir/data/delta >> $topdir/pxb.log 2>&1
 
 run_cmd grep 'InnoDB: xtrabackup: Last MySQL binlog file position' $topdir/pxb.log > $topdir/output
