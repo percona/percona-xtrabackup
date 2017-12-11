3ffedc648227b8b8c32f3fad03c668ae632577bc
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-12-11 19:24:55 +0400

    Last checks on test

--- storage/innobase/xtrabackup/test/t/bug1737179.sh
+++ storage/innobase/xtrabackup/test/t/bug1737179.sh
@@ -27,8 +27,9 @@ xtrabackup --prepare --apply-log-only \
     --target-dir=$topdir/data/full > $topdir/pxb.log 2>&1
 
 vlog "Prepare Increment"
-xtrabackup --prepare  \
+xtrabackup --prepare --apply-log-only  \
     --throttle=40 \
-    --target-dir=$topdir/data/full --incremental-dir=$topdir/data/delta >> $topdir/pxb.log 2>&1
+    --target-dir=$topdir/data/full --incremental-dir=$topdir/data/delta >> $topdir/pxb2.log 2>&1
 
 run_cmd grep 'InnoDB: xtrabackup: Last MySQL binlog file position' $topdir/pxb.log
+run_cmd grep 'InnoDB: xtrabackup: Last MySQL binlog file position' $topdir/pxb2.log
