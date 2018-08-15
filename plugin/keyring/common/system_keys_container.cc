09069e9ee8f84df0ba3091f591b9269842e71503
Sergei Glushchenko <gl.sergei@gmail.com>
2018-08-15 21:44:01 +0700

    PXB-1570: XtraBackup is affected by PS-4519
    - applied Percona Server patch fixing PS-4519
    - applied MySQL patch fixing Bug#26918439

--- plugin/keyring/common/system_keys_container.cc
+++ plugin/keyring/common/system_keys_container.cc
@@ -125,7 +125,7 @@ void System_keys_container::store_or_update_if_system_key_with_version(IKey *key
   if (is_system_key_with_version(key, system_key_id, key_version))
   {
     if (system_key_id_to_system_key.count(system_key_id) == 0) // add a new system key
-      system_key_id_to_system_key.insert(std::make_pair<std::string, System_key_adapter*>(system_key_id, new System_key_adapter(key_version, key)));
+      system_key_id_to_system_key.insert(std::make_pair(system_key_id, new System_key_adapter(key_version, key)));
     else
       update_system_key(key, system_key_id, key_version);
   }
