256c9a1aace568a96e544195c081d1cdd0d83c62
Sergei Glushchenko <gl.sergei@gmail.com>
2019-07-12 16:45:05 +0700

    PXB-1894: PXB crashes during incremental backup prepare for tablespace encryption
    Problem:
    
    When encrypted table have just been created it already has encryption
    flag in its tablespace header, but may not have encryption information
    yet on its first page. This may happen because tablespace flags are
    written synchronously and encryption information goes through the buffer
    pool and will be flushed later.
    
    Xtrabackup will skip such table in hope that it will be created on
    prepare from the redo log. The problem is that crash recovery does not
    actually save encryption information when it see it in the redo
    log. Instead crash recovery only updates in-memory data structures. It
    works in most cases but one, when both full and incremental backups
    happened before InnoDB flushed the first page.
    
    Fix:
    
    Make crash recovery to actually apply log record to the first page when
    encryption information is saved.
    
    Because xtrabackup parsing redo log as it does the backup, it can get
    encryption information for newly created encrypted table from the redo
    log and include its .ibd file into a backup.

--- storage/innobase/log/log0recv.cc
+++ storage/innobase/log/log0recv.cc
@@ -1842,9 +1842,10 @@ recv_parse_or_apply_log_rec_body(
 	        Otherwise, redo will not find the key to decrypt
 		the data pages. */
 		if (page_no == 0 && !is_system_tablespace(space_id)) {
-			return(fil_write_encryption_parse(ptr,
-							  end_ptr,
-							  space_id));
+			if (fil_write_encryption_parse(ptr, end_ptr,
+						       space_id) == NULL) {
+				return(NULL);
+			}
 		}
 		break;
 
