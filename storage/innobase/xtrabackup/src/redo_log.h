a2cbc9209f4f9da4e93a37335944af9be94af575
rahul malik <rahul.malik@percona.com>
2021-01-13 14:20:04 +0530

    Update storage/innobase/xtrabackup/src/redo_log.h
    Co-authored-by: Satya <37100009+satya-bodapati@users.noreply.github.com>

--- storage/innobase/xtrabackup/src/redo_log.h
+++ storage/innobase/xtrabackup/src/redo_log.h
@@ -43,8 +43,8 @@ class Redo_Log_Reader {
   @return true if success. */
   bool find_start_checkpoint_lsn();
 
-  /** Check if redo logs were disabled during the backup.
-   @return true if success. */
+  /** Check if redo logs are disabled during the backup.
+  @return true if redo logs are disabled */
   bool validate_redo_log_file();
 
   /** Get log header. */
