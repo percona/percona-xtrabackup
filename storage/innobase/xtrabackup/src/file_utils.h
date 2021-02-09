5a00e40e18dfc9e544493937822c9f799da8eba6
Luis Donoso <luis.donoso@percona.com>
2021-02-09 13:46:11 +0100

    PXB-2371 - Vulnerability: Path traversal
    Modify xbstream so it create/extracts streams as gnu tar does.
    
    When creating a stream, the leading directory slashes and relative
    directories are removed from the filename. Hence, when the stream is
    extracted, the files will be created in the directory specified, e.i.
    its new path is relative.
    
    The parameter `-P`, or `--absolute-names`, has been added to allow
    absolute paths.
    
    It is an error if there is a file with an absolute path when extracting
    a stream and the parameter `-P` has not been specified.
    
    PXB-2371-2.4 - Fix formatting

---
+++ storage/innobase/xtrabackup/src/file_utils.h
@@ -0,0 +1,28 @@
+/******************************************************
+Copyright (c) 2021 Percona LLC and/or its affiliates.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+*******************************************************/
+
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+/* Return a safer suffix of FILE_NAME, or "." if it has no safer
+   suffix.  Check for fully specified file names and other atrocities.
+   Warn the user if we do not return NAME. */
+/* Based on gnu tar */
+const char *safer_name_suffix(char const *file_name, int *prefix_len_out);
+
+#endif
