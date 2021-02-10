1dd8ef861bf4b668abc67ca8fab9fb6fd9805f6f
Yura Sorokin <yura.sorokin@percona.com>
2021-02-10 02:53:01 +0200

    PS-5364 feature: Update Hashicorp Vault plugin to use v2 API (MP listing cleanup)
    https://jira.percona.com/browse/PS-5364
    
    Removed 'parse_mount_point_version()' method from the 'IVault_parser_composer'
    interface / 'Vault_parser_composer' class as it is no longer needed for mount
    point secret engine auto-detection.
    
    The following related gunit tests were removed:
    - 'Vault_parser_test.GetVaultVersion2';
    - 'Vault_parser_test.GetVaultVersion1';
    - 'Vault_parser_test.GetVaultOptionsNull';
    - 'Vault_parser_test.GetVaultOptionsEmpty'.
    
    Introduced the following new gunit tests to cover mount point config parsing
    functionality:
    - 'Vault_parser_test.GetMountConfig';
    - 'Vault_parser_test.GetMountConfigNull';
    - 'Vault_parser_test.GetMountConfigIncomplete'.
    
    (cherry picked from commit 00d9409c3f56d2f427b8e839c9f5bdb4f0735de4)

--- plugin/keyring_vault/vault_parser_composer.h
+++ plugin/keyring_vault/vault_parser_composer.h
@@ -39,11 +39,6 @@ class Vault_parser_composer : public IVault_parser_composer {
   virtual bool parse_errors(const Secure_string &payload,
                             Secure_string *      errors);
 
-  virtual bool parse_mount_point_version(
-      const Secure_string &secret_mount_point,
-      const Secure_string &mount_points_payload,
-      Vault_version_type &vault_version, Secure_string &mount_point_path,
-      Secure_string &directory_path);
   virtual bool parse_mount_point_config(const Secure_string &config_payload,
                                         std::size_t &        max_versions,
                                         bool &               cas_required,
