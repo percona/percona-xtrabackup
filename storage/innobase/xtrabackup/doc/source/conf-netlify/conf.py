8eb238aa5e464bc3d290caa572f29694d9d96c98
patrick.birch <patrick.birch@percona.com>
2021-10-04 13:59:31 -0500

    PXB-2616 Update netlify.toml (8.0)
    Makefile
    netlify.toml
    requirements.txt
    runtime.txt
    source/conf-netlify

---
+++ storage/innobase/xtrabackup/doc/source/conf-netlify/conf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.abspath("../"))
+from conf import *
+extensions.append('sphinx_gitstamp')
+extensions.append('sphinx_copybutton')
+html_sidebars['**']=['globaltoc.html', 'searchbox.html', 'localtoc.html', 'logo-text.html']
+html_theme = 'sphinx_material'
+html_theme_options = {
+    'base_url': 'http://bashtage.github.io/sphinx-material/',
+    'repo_url': 'https://github.com/percona/percona-xtrabackup',
+    'repo_name': 'percona/percona-xtrabackup',
+    'color_accent': 'grey',
+    'color_primary': 'orange',
+    'globaltoc_collapse': True,
+    'version_dropdown': True,
+    'version_dropdown_text': 'Versions',
+    'version_info': {
+        "2.4": "https://percona-xtrabackup-24.netlify.app/",
+        "8.0": "https://percona-xtrabackup-80.netlify.app/",
+        "Latest": "https://percona-xtrabackup-80.netlify.app/"
+    },
+}
+html_logo = '../_static/images/percona-logo.svg'
+html_favicon = '../_static/images/percona_favicon.ico'
+pygments_style = 'emacs'
+gitstamp_fmt = "%b %d, %Y"
+copybutton_prompt_text = '$'
+#html_last_updated_fmt = ''
