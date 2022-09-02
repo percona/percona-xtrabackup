percona/percona-xtrabackup

commit 907cf3d99b9089c1ac892269ec38ec41ea97f638

8.0.30 merge fixed test cases
removed srv_read_only_mode from recv_apply_hashed_log_recs
since it created crash xb_stat.sh
removed code of moving old redo files during restore
PXB calls log_crash_safe_validate to check if redo log is disabled

and few fixes in test cases

storage/innobase/xtrabackup/src/xtrabackup.cc
@@ -4879,25 +4879,19 @@ static void xtrabackup_stats_func(int argc, char **argv) {
 
   /* Check if the log files have been created, otherwise innodb_init()
   will crash when called with srv_read_only == true */
-  for (n = 0; n < srv_log_n_files; n++) {
-    char logname[FN_REFLEN];
-    bool exists;
-    os_file_type_t type;
+  auto log_files_ctx =
+      Log_files_context{srv_log_group_home_dir, Log_files_ruleset::CURRENT};
+  ut::vector<Log_file_id> listed_files;
+  const dberr_t err = log_list_existing_files(log_files_ctx, listed_files);
 
-    snprintf(logname, sizeof(logname), "%s%c%s%lu", srv_log_group_home_dir,
-             OS_PATH_SEPARATOR, "ib_logfile", (ulong)n);
-    Fil_path::normalize(logname);
-
-    if (!os_file_status(logname, &exists, &type) || !exists ||
-        type != OS_FILE_TYPE_FILE) {
-      xb::error() << "Cannot find log file " << logname;
-      xb::error() << "to use the statistics feature, you need a "
-                     "clean copy of the database including "
-                     "correctly sized log files, so you need to "
-                     "execute with --prepare twice to use this "
-                     "functionality on a backup.";
-      exit(EXIT_FAILURE);
-    }
+  if (err != DB_SUCCESS) {
+    xb::error() << "Cannot find log files ";
+    xb::error() << "to use the statistics feature, you need a "
+                   "clean copy of the database including "
+                   "correctly sized log files, so you need to "
+                   "start server once after prepare to use "
+                   "functionality on a backup.";
+    exit(EXIT_FAILURE);
   }
 
   xb::info() << "Starting 'read-only' InnoDB instance to gather "
@@ -4906,6 +4900,9 @@ static void xtrabackup_stats_func(int argc, char **argv) {
              << " bytes for buffer pool (set by "
              << ((predict_memory) ? "--use-free-memory-pct" : "--use-memory")
              << " parameter)";
+
+  srv_redo_log_capacity = 1024 * 1024 * 1024;
+  srv_redo_log_capacity_used = srv_redo_log_capacity;
 
   if (innodb_init(true, false)) exit(EXIT_FAILURE);
 

storage/innobase/srv/srv0start.cc
@@ -2105,12 +2105,8 @@ dberr_t srv_start(bool create_new_db, lsn_t to_lsn) {
       would write new redo records in the current fmt,
       and end up with file in both formats = invalid. */
 
-      err = recv_apply_hashed_log_recs(
-          *log_sys, !recv_sys->is_cloned_db && !log_upgrade
-#ifdef XTRABACKUP
-                        && !srv_read_only_mode
-#endif
-      );
+      err = recv_apply_hashed_log_recs(*log_sys,
+                                       !recv_sys->is_cloned_db && !log_upgrade);
 
       if (recv_sys->found_corrupt_log || err != DB_SUCCESS) {
         err = DB_ERROR;

storage/innobase/xtrabackup/src/backup_copy.cc
@@ -2316,7 +2316,7 @@ bool copy_back(int argc, char **argv) {
     ds_data = NULL;
   }
 
-  /* copy redo logs */
+  /* create #innodb_redo */
 
   dst_dir = (srv_log_group_home_dir && *srv_log_group_home_dir)
                 ? srv_log_group_home_dir
@@ -2324,16 +2324,17 @@ bool copy_back(int argc, char **argv) {
 
   ds_data = ds_create(dst_dir, DS_TYPE_LOCAL);
 
-  for (long i = 0; i < innobase_log_files_in_group; i++) {
-    char filename[30];
-    sprintf(filename, "ib_logfile%ld", i);
+  if (directory_exists(LOG_DIRECTORY_NAME, false)) {
+    char errbuf[MYSYS_STRERROR_SIZE];
+    dst_dir = (srv_log_group_home_dir && *srv_log_group_home_dir)
+                  ? srv_log_group_home_dir
+                  : mysql_data_home;
+    std::string dest_redo_dir =
+        std::string(dst_dir) + FN_DIRSEP + LOG_DIRECTORY_NAME;
+    if (mkdirp(dest_redo_dir.c_str(), 0777, MYF(0)) < 0) {
+      xb::error() << "Can not create directory " << dest_redo_dir << ": "
+                  << my_strerror(errbuf, sizeof(errbuf), my_errno());
 
-    if (!file_exists(filename)) {
-      continue;
-    }
-
-    if (!(ret = copy_or_move_file(filename, filename, dst_dir, 1,
-                                  FILE_PURPOSE_REDO_LOG))) {
       goto cleanup;
     }
   }
@@ -2414,7 +2415,7 @@ bool copy_back(int argc, char **argv) {
                          "copy-back");
   if (!ret) goto cleanup;
 
-  /* copy buufer pool dump */
+  /* copy buffer pool dump */
   if (innobase_buffer_pool_filename) {
     const char *src_name;
     char path[FN_REFLEN];
@@ -2502,21 +2503,6 @@ bool copy_back(int argc, char **argv) {
 
       ds_destroy(ds_data);
       ds_data = nullptr;
-    }
-  }
-  /* create #innodb_redo */
-  if (directory_exists(LOG_DIRECTORY_NAME, false)) {
-    char errbuf[MYSYS_STRERROR_SIZE];
-    dst_dir = (innobase_data_home_dir && *innobase_data_home_dir)
-                  ? innobase_data_home_dir
-                  : mysql_data_home;
-    std::string dest_redo_dir =
-        std::string(dst_dir) + FN_DIRSEP + LOG_DIRECTORY_NAME;
-    if (mkdirp(dest_redo_dir.c_str(), 0777, MYF(0)) < 0) {
-      xb::error() << "Can not create directory " << dest_redo_dir << ": "
-                  << my_strerror(errbuf, sizeof(errbuf), my_errno());
-
-      return (false);
     }
   }
 

storage/innobase/xtrabackup/src/redo_log.cc
@@ -88,17 +88,6 @@ bool Redo_Log_Reader::find_start_checkpoint_lsn() {
   return (true);
 }
 
-bool Redo_Log_Reader::validate_redo_log_file() {
-  /* Look for the latest checkpoint */
-  Log_checkpoint_location checkpoint;
-
-  if (!recv_find_max_checkpoint(*log_sys, checkpoint)) {
-    xb::error() << " recv_find_max_checkpoint() failed.";
-    return (false);
-  }
-  return (true);
-}
-
 byte *Redo_Log_Reader::get_header() const { return log_hdr_buf; }
 
 byte *Redo_Log_Reader::get_buffer() const { return log_buf; }
@@ -1165,10 +1154,10 @@ bool Redo_Log_Data_Manager::stop_at(lsn_t lsn, lsn_t checkpoint_lsn) {
 
   archived_log_monitor.stop();
 
-  /* check if redo log are disabled */
-  if (!reader.validate_redo_log_file()) {
-    return (false);
-  }
+  /* to ensure redo logs are not disabled during the backup, reopen the log
+  files to read HEADER */
+  reopen_log_files();
+  log_crash_safe_validate(*log_sys);
 
   scanned_lsn = reader.get_scanned_lsn();
 

storage/innobase/xtrabackup/src/redo_log.h
@@ -43,10 +43,6 @@ class Redo_Log_Reader {
   @return true if success. */
   bool find_start_checkpoint_lsn();
 
-  /** Check if redo logs are disabled during the backup.
-  @return true if redo logs are disabled */
-  bool validate_redo_log_file();
-
   /** Get log header. */
   byte *get_header() const;
 

storage/innobase/xtrabackup/test/t/alter_instance_redo.sh
@@ -35,7 +35,6 @@ run_cmd_expect_failure xtrabackup --backup --target-dir=$topdir/inc \
 	--incremental-basedir=$topdir/full 2>&1 \
 	| tee $topdir/inc.log
 grep "Redo logging is disabled, cannot take consistent backup" $topdir/inc.log || die "missing error message"
-rm -r $topdir/inc
 rm -r $topdir/full
 rm  $topdir/inc.log
 mysql -e "alter instance enable innodb redo_log"

storage/innobase/xtrabackup/test/t/bug1512616.sh
@@ -23,7 +23,7 @@ rm -rf ${TEST_VAR_ROOT}/dir
 
 xtrabackup --move-back --target-dir=$topdir/backup
 
-if [ ! -f ${TEST_VAR_ROOT}/dir/logs/ib_logfile0 ] ; then
+if [ ! -d ${TEST_VAR_ROOT}/dir/logs/#innodb_redo] ; then
 	die "Log files were not moved to correct place!"
 fi
 

storage/innobase/xtrabackup/test/t/bug1669592.sh
@@ -21,10 +21,7 @@ xtrabackup --backup --target-dir=$topdir/backup-inc --incremental-basedir=$topdi
 xtrabackup --prepare --apply-log-only --target-dir=$topdir/backup
 xtrabackup --prepare --incremental-dir=$topdir/backup-inc --target-dir=$topdir/backup
 
-test -f $topdir/backup/ib_logfile0 || die "Log files are not found in full backup directory"
-
-# force MySQL to check system tablespace for consistency
-rm -rf $topdir/backup/ib_logfile*
+test -d $topdir/backup/#innodb_redo || die "redo directory are not found in full backup directory"
 
 stop_server
 

commit d7dc1d70b6a618c4dc71c67a31dd30e1bfac4978

8.0.30 merge - stabilize test case
Adjusted test to skip in case server has no support for buffer pool
dump

Fixed compilation issue introduced in 907cf3d.

storage/innobase/xtrabackup/src/xtrabackup.cc
@@ -4830,8 +4830,6 @@ static void stat_with_rec(dict_table_t *table, THD *thd,
 }
 
 static void xtrabackup_stats_func(int argc, char **argv) {
-  ulint n;
-
   /* cd to datadir */
 
   if (my_setwd(mysql_real_data_home, MYF(MY_WME))) {

storage/innobase/xtrabackup/test/t/PXB-1548_basic.sh
@@ -19,6 +19,9 @@ mkdir $topdir/backup
 
 xtrabackup --backup --dump-innodb-buffer-pool --target-dir=$topdir/backup
 
+grep -q "Server has no support for innodb_buffer_pool_dump_now" $OUTFILE \
+&& skip_test "Server has no support for innodb_buffer_pool_dump_now"
+
 if [[ ! -f  $mysql_datadir/ib_buffer_pool ]]; then
     die "dump file doesnt exist"
 fi

storage/innobase/xtrabackup/test/t/PXB-1548_rsync_basic.sh
@@ -22,6 +22,8 @@ mkdir $topdir/backup
 
 xtrabackup --backup --dump-innodb-buffer-pool --rsync --target-dir=$topdir/backup
 
+grep -q "Server has no support for innodb_buffer_pool_dump_now" $OUTFILE \
+&& skip_test "Server has no support for innodb_buffer_pool_dump_now"
 
 if [[ ! -f  $mysql_datadir/ib_buffer_pool ]]; then
     die "dump file doesnt exist"