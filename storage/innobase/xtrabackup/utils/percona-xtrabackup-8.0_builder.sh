31a604b114c0d1ac376c9bd4a62bac08f71d5e4d
Vadim Yalovets <vadim.yalovets@percona.com>
2023-03-17 16:13:08 +0200

    RM-1164-PXB-8.0.32-26

--- storage/innobase/xtrabackup/utils/percona-xtrabackup-8.0_builder.sh
+++ storage/innobase/xtrabackup/utils/percona-xtrabackup-8.0_builder.sh
@@ -243,7 +243,7 @@ install_deps() {
             PKGLIST+=" binutils-devel python3-pip python3-setuptools"
             PKGLIST+=" libcurl-devel cmake libaio-devel zlib-devel libev-devel bison make gcc"
             PKGLIST+=" rpm-build libgcrypt-devel ncurses-devel readline-devel openssl-devel gcc-c++"
-            PKGLIST+=" vim-common rpmlint patchelf python3-wheel"
+            PKGLIST+=" vim-common rpmlint patchelf python3-wheel libudev-devel"
             if [ $RHEL = 9 ]; then
                 PKGLIST+=" rsync procps-ng-devel python3-sphinx"
             else
