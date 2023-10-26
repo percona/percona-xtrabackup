8795b89a08128899735a1aac092c542f774fb54c
Marcelo Altmann <marcelo.altmann@percona.com>
2023-10-26 13:26:28 -0300

    PXB-3185: Merge 8.2 - Fix Compilation issues
    CMake Error at cmake/mysql_version.cmake:65 (MYSQL_GET_CONFIG_VALUE):
      MYSQL_GET_CONFIG_VALUE Macro invoked with incorrect arguments for macro
      named: MYSQL_GET_CONFIG_VALUE
    Call Stack (most recent call first):
      cmake/mysql_version.cmake:218 (GET_MYSQL_VERSION)
      CMakeLists.txt:577 (INCLUDE)
    
    1159ccb - Added a new entry into MYSQL_VERSION file. PXB has a custom
    MYSQL_GET_CONFIG_VALUE macro which accepts a 3rd parameter to instruct
    which file we are parsing from. Adjuster MYSQL_VERSION_STABILITY
    to be read from MYSQL_VERSION.
    
    /work/pxb/src/8.0/storage/innobase/xtrabackup/src/xtrabackup.cc: In function ‘void init_mysql_environment()’:
    /work/pxb/src/8.0/storage/innobase/xtrabackup/src/xtrabackup.cc:3924:3: error: ‘randominit’ was not declared in this scope; did you mean ‘random_r’?
     3924 |   randominit(&sql_rand, server_start_time, server_start_time / 2);
          |   ^~~~~~~~~~
          |   random_r
    
    6fee62f moved the declaration of randominit from include/mysql_com.h to include/my_rnd.h

--- storage/innobase/xtrabackup/src/xtrabackup.cc
+++ storage/innobase/xtrabackup/src/xtrabackup.cc
@@ -42,7 +42,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 #include <my_base.h>
 #include <my_default.h>
 #include <my_getopt.h>
-#include <mysql_com.h>
+#include <my_rnd.h>
 #include <mysql_version.h>
 #include <mysqld.h>
 #include "log0files_io.h"
