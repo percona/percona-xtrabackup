67b7bc34ea99549ef8a9ab7c337b9a01f8e23e8b
zhaoxinyu <zhaoxinyu512@gmail.com>
2024-07-24 16:58:03 +0800

    Fix fts index pattern typo

--- storage/innobase/xtrabackup/src/backup_mysql.cc
+++ storage/innobase/xtrabackup/src/backup_mysql.cc
@@ -2180,7 +2180,7 @@ bool is_fts_index(const std::string &table_name) {
   const char *pattern =
       "^(FTS|fts)_[0-9a-fA-f]{16}_(([0-9a-fA-]{16}_(INDEX|index)_[1-6])|"
       "DELETED_CACHE|deleted_cache|DELETED|deleted|CONFIG|config|BEING_DELETED|"
-      "being_deleted|BEING_DELETED_CACHE|beign_deleted_cache)$";
+      "being_deleted|BEING_DELETED_CACHE|being_deleted_cache)$";
   const char *error_context = "is_fts_index";
   return check_regexp_table_name(table_name, error_context, pattern);
 }
