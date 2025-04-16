29dbe47e62a5fcd6b295e6d9592db9c2b6df0ff9
aibek.bukabayev aibek.bukabayev@percona.com <aibek.bukabayev@percona.com>
2025-04-16 10:12:17 +0000

    Fix rename bug This covers a case when we have 2 RENAME DDLs on the same table, first rename happens before the files are discovered second rename happens after files discovered but before files are opened for loading to cache

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -12215,6 +12215,7 @@ dberr_t Tablespace_dirs::scan(bool populate_fil_cache IF_XB(, bool only_undo)) {
 
 #ifdef XTRABACKUP
   debug_sync_point("xtrabackup_suspend_between_file_discovery_and_open");
+  debug_sync_thread("xtrabackup_suspend_between_file_discovery_and_open");
 #endif /* XTRABACKUP */
 
   if (err == DB_SUCCESS && populate_fil_cache IF_XB(&&!only_undo)) {
