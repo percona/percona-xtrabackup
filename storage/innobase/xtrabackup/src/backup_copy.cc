11a2ee89fd82b824340861bc1482fca10d7236bf
Satya Bodapati <satya.bodapati@percona.com>
2025-06-06 15:47:18 +0530

    Revert "[8.0] PXB-3470: xbstream does long post-processing after receiving al…"

--- storage/innobase/xtrabackup/src/backup_copy.cc
+++ storage/innobase/xtrabackup/src/backup_copy.cc
@@ -2601,8 +2601,7 @@ bool decrypt_decompress_file(const char *filepath, uint thread_n) {
   }
   if (ds_data->fs_support_punch_hole) {
     char error[512];
-    if (!restore_sparseness(dest_filepath, opt_read_buffer_size,
-                            error IF_DEBUG(, true))) {
+    if (!restore_sparseness(dest_filepath, opt_read_buffer_size, error)) {
       xb::warn() << "restore_sparseness failed for file: " << dest_filepath
                  << " Error: " << error;
     }
