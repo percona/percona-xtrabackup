0bf86269abd7cb63e033fbdc81eaf9dbddc38905
Vadim Yalovets <vadim.yalovets@percona.com>
2025-08-05 18:06:26 +0300

    PKG-1030 Packaging tasks for release - PXB 8.4.0-4

--- storage/innobase/xtrabackup/utils/percona-xtrabackup-8.0_builder.sh
+++ storage/innobase/xtrabackup/utils/percona-xtrabackup-8.0_builder.sh
@@ -272,7 +272,7 @@ install_deps() {
             PKGLIST+=" rpm-build libgcrypt-devel ncurses-devel readline-devel openssl-devel"
             PKGLIST+=" vim-common rpmlint patchelf python3-wheel libudev-devel"
             if [ ${RHEL} = 9 -o ${RHEL} = 10 ]; then
-                PKGLIST+=" rsync procps-ng-devel python3-sphinx gcc gcc-c++"
+                PKGLIST+=" rsync procps-ng-devel python3-sphinx gcc gcc-c++ gcc-gfortran"
             else
                 if [ x"$ARCH" = "xx86_64" ]; then
                     yum-config-manager --enable powertools
