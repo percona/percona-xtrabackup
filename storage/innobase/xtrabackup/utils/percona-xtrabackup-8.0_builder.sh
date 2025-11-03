f7e9234db78f04c067f2f5ce01fb5cec96e591b4
Vadim Yalovets <vadim.yalovets@percona.com>
2025-11-03 13:12:35 +0200

    CUSTOM-228 Hotfix build with the fix for PXB-2724

--- storage/innobase/xtrabackup/utils/percona-xtrabackup-8.0_builder.sh
+++ storage/innobase/xtrabackup/utils/percona-xtrabackup-8.0_builder.sh
@@ -285,7 +285,7 @@ install_deps() {
         apt-get update
 
         PKGLIST+=" bison cmake devscripts debconf debhelper automake bison ca-certificates libcurl4-openssl-dev"
-        PKGLIST+=" cmake debhelper libaio-dev libncurses-dev libtool libz-dev libsasl2-dev"
+        PKGLIST+=" cmake debhelper libaio-dev libncurses-dev libtool libz-dev libsasl2-dev vim-common"
         PKGLIST+=" libgcrypt-dev libev-dev lsb-release libudev-dev"
         PKGLIST+=" build-essential rsync libdbd-mysql-perl libnuma1 socat libssl-dev patchelf libicu-dev"
 
